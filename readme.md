587534ea0ec223d31211bf5fd2cde607df127bb1
chenxun <594780795@qq.com>
2019-12-03 11:59:07 +0800

    This is a place holder.

---
+++ readme.md
@@ -0,0 +1,10 @@
+# ava_data_preproc
+
+AVA数据集标注及预处理工具链。
+
+
+注:
+* Bare git repo 位置: 10.0.14.49/home/wniu/ava_data_preproc/
+* AVA数据位置: /data/video_caption_database/ava/ava/
+* 代标注数据位置: /data/video_caption_database/XiaMen/TrainVedio/behavior_recognition/
+
---
+++ vid_fallDown/copy_file_in.sh
@@ -0,0 +1,6 @@
+cp /data/video_caption_database/XiaMen/TrainVedio/behavior_recognition/8摔倒/fall1.mp4 .
+cp /data/video_caption_database/XiaMen/TrainVedio/behavior_recognition/8摔倒/fall2.mp4 .
+cp /data/video_caption_database/XiaMen/TrainVedio/behavior_recognition/8摔倒/fall3.mp4 .
+cp /data/video_caption_database/XiaMen/TrainVedio/behavior_recognition/8摔倒/fall4.mp4 .
+cp /data/video_caption_database/XiaMen/TrainVedio/behavior_recognition/8摔倒/fall5.mp4 .
+cp /data/video_caption_database/XiaMen/TrainVedio/behavior_recognition/8摔倒/fall6.mp4 .
