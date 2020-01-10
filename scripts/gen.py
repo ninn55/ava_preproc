03980ebc1e467b085c59d24c89a959457a71a26b
niuwenxu <wniu@connect.ust.hk>
2020-01-10 08:37:09 +0800

    Update

---
+++ scripts/gen.py
@@ -0,0 +1,27 @@
+from comutil import *
+
+class Gen():
+    def __init__(self, videodir: str, frameloc: list, vidduration: dict, ):
+        self.__videolist = None
+        self.__vidduration = None
+        self.__frameloc = None
+
+    def excesscheck(self):
+        pass
+    def gen_vidduration(self):
+        pass
+
+class GenKeyframes(Gen):
+    pass
+    def getcommand(self):
+        pass
+        yield
+
+class GenClips(Gen):
+    pass
+    def getcommand(self):
+        pass
+        yield
+
+if __name__ == "__main__":
+    pass
